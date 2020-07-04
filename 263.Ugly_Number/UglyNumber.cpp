9275f904a7b51957962f8c7dc8e5b1d2bf00f89c
MrinmoiHossain <mokit.aust@gmail.com>
2020-07-04 14:18:15 +0600

    ugly number check

---
+++ 263.Ugly_Number/UglyNumber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        while(num > 1){
+            if(num % 2 == 0)
+                num /= 2;
+            else if(num % 3 == 0)
+                num /= 3;
+            else if(num % 5 == 0)
+                num /= 5;
+            else
+                return false;
+        }
+        
+        return (num == 1);
+    }
+};
