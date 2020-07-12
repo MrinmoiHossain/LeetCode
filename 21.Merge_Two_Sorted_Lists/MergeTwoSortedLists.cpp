c860e81e61daf98e079491385aac4832b01eeac9
MrinmoiHossain <mokit.aust@gmail.com>
2020-07-12 06:01:30 +0600

    linked list

---
+++ 21.Merge_Two_Sorted_Lists/MergeTwoSortedLists.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode();
+        ListNode* current = head;
+        
+        while(l1 != NULL && l2 != NULL){
+            if(l1->val < l2->val){
+                current->next = l1;
+                l1 = l1->next;
+            }
+            else{
+                current->next = l2;
+                l2 = l2->next;
+            }
+            current = current->next;
+        }
+        
+        if(l1)
+            current->next = l1;
+        else
+            current->next = l2;
+        
+        return head->next;
+    }
+};
