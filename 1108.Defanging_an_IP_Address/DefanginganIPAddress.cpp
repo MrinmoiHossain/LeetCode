995489db5aabbff8080a7b0ddf86703d5c301a3a
MrinmoiHossain <mokit.aust@gmail.com>
2020-08-01 03:57:37 +0600

    string

---
+++ 1108.Defanging_an_IP_Address/DefanginganIPAddress.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string s = "";
+        for(auto c : address){
+            if(c == '.'){
+                s += "[.]";
+            }
+            else{
+                s += c;
+            }
+        }
+        
+        return s;
+    }
+};
